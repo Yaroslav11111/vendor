e08e55fd0576507bbc661da9b805e7c4dcb4de4b
Yaroslav <y.snigko@bossgs.org>
2018-01-29 10:59:41 +0200

    first commit

---
+++ yiisoft/extensions.php
@@ -0,0 +1,177 @@
+<?php
+
+$vendorDir = dirname(__DIR__);
+
+return array (
+  'asofter/yii2-imperavi-redactor' => 
+  array (
+    'name' => 'asofter/yii2-imperavi-redactor',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/imperavi' => $vendorDir . '/asofter/yii2-imperavi-redactor/yii/imperavi',
+    ),
+  ),
+  'yiisoft/yii2-jui' => 
+  array (
+    'name' => 'yiisoft/yii2-jui',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/jui' => $vendorDir . '/yiisoft/yii2-jui',
+    ),
+  ),
+  'mihaildev/yii2-elfinder' => 
+  array (
+    'name' => 'mihaildev/yii2-elfinder',
+    'version' => '1.2.2.0',
+    'alias' => 
+    array (
+      '@mihaildev/elfinder' => $vendorDir . '/mihaildev/yii2-elfinder',
+    ),
+  ),
+  'trntv/yii2-aceeditor' => 
+  array (
+    'name' => 'trntv/yii2-aceeditor',
+    'version' => '2.0.2.0',
+    'alias' => 
+    array (
+      '@trntv/aceeditor' => $vendorDir . '/trntv/yii2-aceeditor/src',
+    ),
+  ),
+  'trntv/yii2-command-bus' => 
+  array (
+    'name' => 'trntv/yii2-command-bus',
+    'version' => '2.1.3.0',
+    'alias' => 
+    array (
+      '@trntv/bus' => $vendorDir . '/trntv/yii2-command-bus/src',
+    ),
+  ),
+  'trntv/yii2-datetime-widget' => 
+  array (
+    'name' => 'trntv/yii2-datetime-widget',
+    'version' => '1.0.4.0',
+    'alias' => 
+    array (
+      '@trntv/yii/datetime' => $vendorDir . '/trntv/yii2-datetime-widget/src',
+    ),
+  ),
+  'trntv/yii2-file-kit' => 
+  array (
+    'name' => 'trntv/yii2-file-kit',
+    'version' => '1.2.1.0',
+    'alias' => 
+    array (
+      '@trntv/filekit' => $vendorDir . '/trntv/yii2-file-kit/src',
+    ),
+  ),
+  'trntv/yii2-glide' => 
+  array (
+    'name' => 'trntv/yii2-glide',
+    'version' => '1.2.0.0',
+    'alias' => 
+    array (
+      '@trntv/glide' => $vendorDir . '/trntv/yii2-glide/src',
+    ),
+  ),
+  'yiisoft/yii2-httpclient' => 
+  array (
+    'name' => 'yiisoft/yii2-httpclient',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/httpclient' => $vendorDir . '/yiisoft/yii2-httpclient',
+    ),
+  ),
+  'yiisoft/yii2-authclient' => 
+  array (
+    'name' => 'yiisoft/yii2-authclient',
+    'version' => '2.1.4.0',
+    'alias' => 
+    array (
+      '@yii/authclient' => $vendorDir . '/yiisoft/yii2-authclient',
+    ),
+  ),
+  'yiisoft/yii2-swiftmailer' => 
+  array (
+    'name' => 'yiisoft/yii2-swiftmailer',
+    'version' => '2.1.0.0',
+    'alias' => 
+    array (
+      '@yii/swiftmailer' => $vendorDir . '/yiisoft/yii2-swiftmailer',
+    ),
+  ),
+  'yiisoft/yii2-codeception' => 
+  array (
+    'name' => 'yiisoft/yii2-codeception',
+    'version' => '2.0.6.0',
+    'alias' => 
+    array (
+      '@yii/codeception' => $vendorDir . '/yiisoft/yii2-codeception',
+    ),
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '2.0.7.0',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap',
+    ),
+  ),
+  'yiisoft/yii2-faker' => 
+  array (
+    'name' => 'yiisoft/yii2-faker',
+    'version' => '2.0.3.0',
+    'alias' => 
+    array (
+      '@yii/faker' => $vendorDir . '/yiisoft/yii2-faker',
+    ),
+  ),
+  'yiisoft/yii2-gii' => 
+  array (
+    'name' => 'yiisoft/yii2-gii',
+    'version' => '2.0.5.0',
+    'alias' => 
+    array (
+      '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii',
+    ),
+  ),
+  'yiier/yii2-chartjs-widget' => 
+  array (
+    'name' => 'yiier/yii2-chartjs-widget',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@yiier/chartjs' => $vendorDir . '/yiier/yii2-chartjs-widget',
+    ),
+  ),
+  '2amigos/yii2-chartjs-widget' => 
+  array (
+    'name' => '2amigos/yii2-chartjs-widget',
+    'version' => '2.1.2.0',
+    'alias' => 
+    array (
+      '@dosamigos/chartjs' => $vendorDir . '/2amigos/yii2-chartjs-widget/src',
+    ),
+  ),
+  'fedemotta/yii2-cronjob' => 
+  array (
+    'name' => 'fedemotta/yii2-cronjob',
+    'version' => '1.1.0.0',
+    'alias' => 
+    array (
+      '@fedemotta/cronjob' => $vendorDir . '/fedemotta/yii2-cronjob',
+    ),
+  ),
+  'yiisoft/yii2-debug' => 
+  array (
+    'name' => 'yiisoft/yii2-debug',
+    'version' => '2.0.13.0',
+    'alias' => 
+    array (
+      '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug',
+    ),
+  ),
+);
